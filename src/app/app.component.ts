b0f7aca1bc40a635945bf8dc9a976911aebecf9b
rOBI <robertaliaj97@gmail.com>
2023-03-08 21:45:24 +0100

    Added images and Slide Show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'robertaliaj.de';
+export class AppComponent implements OnInit {
+  images = ['Asude.jpg', 'Grieß.jpg', 'Lchstatar.jpg', 'Schoki.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImg();
+  }
+
+  updateImg() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
 }
